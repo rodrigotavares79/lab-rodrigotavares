07a218e731459bba816030513f0d49490dc950d9
rodrigotavares79 <tavaresrodrigo@gmail.com>
2026-08-31 11:27:16 -0300

    Update page.tsx

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,1 +1,17 @@
+import { SignIn } from "@clerk/nextjs";
 
+export default function SignInPage() {
+  return (
+    <div
+      style={{
+        minHeight: "80vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        padding: "3rem 1.5rem",
+      }}
+    >
+      <SignIn routing="hash" />
+    </div>
+  );
+}
